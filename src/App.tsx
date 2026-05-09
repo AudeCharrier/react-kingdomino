9ab01f35f4913d523e07d309ecc524cbb2fd18f8
AUDE CHARRIER <charrier.aude@gmail.com>
2026-05-09 10:42:56 +0200

    chore:move tile assets to public folder for API access

--- src/App.tsx
+++ src/App.tsx
@@ -204,7 +204,17 @@ function App() {
 			}}
 		>
 			<header className="kingdo-header">
+				<div className="reset-game">
+					<p>↑ Rafraîchis la page pour rejouer</p>
+					<p> (Bouton Reset coming soon !)</p>
+				</div>
 				<h1>Kingdomino Origins</h1>
+				<div className="credits">
+					<p>Un jeu de Bruno Cathala</p>
+					<a href="https://www.youtube.com/watch?v=e6UFh9tLAiA">
+						La règle en vidéo pour plusieurs joueurs
+					</a>
+				</div>
 			</header>
 			{showOverlay && <StartOverlay onStart={handleCloseOverlay} />}
 			<div
